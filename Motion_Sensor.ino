29df27dc9ba68ffb743d6232aa9751497217ff09
Miles15 <mcollins2@oxy.edu>
2016-02-29 14:30:51 -0800

    Added files via upload

---
+++ Motion_Sensor.ino
@@ -0,0 +1,42 @@
+int trigPin = 12;
+int echoPin = 11;
+int led = 10;
+
+void setup() 
+{
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+}
+void loop()
+{
+  int duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+
+  if (distance >= 400 || distance <= 2)
+  {
+    Serial.println("Out of range");
+    digitalWrite(led,LOW);
+  }
+  else if(distance <= 20)
+  {
+    Serial.print(distance);
+    Serial.println(" cm");
+    digitalWrite(led,HIGH);
+    
+  }
+  else
+  {
+    Serial.print(distance);
+    Serial.println(" cm");
+    digitalWrite(led, LOW);
+  }
+  delay(500);
+}
